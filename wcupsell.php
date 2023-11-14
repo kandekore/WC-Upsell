554d19c8446c0f6cd0e14d702007032a973ba3ae
kandekore <darren@kandekore.net>
2023-11-14 17:03:45 +0000

    Set Order

--- wcupsell.php
+++ wcupsell.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Upsell Popup
  * Description: An upsell popup for WooCommerce checkout.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: D Kandekore
  */
 
@@ -15,26 +15,26 @@ function woo_upsell_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'woo_upsell_enqueue_scripts');
 
-// Settings Page
-function upsell_add_settings_page() {
-    add_options_page(
-        'Upsell Settings',
-        'Upsell Settings',
+add_action('admin_menu', 'woo_upsell_admin_menu');
+
+function woo_upsell_admin_menu() {
+    add_menu_page(
+        'WooCommerce Upsell Popup Settings',
+        'Upsell Popup Settings',
         'manage_options',
-        'upsell-settings',
-        'upsell_render_settings_page'
+        'woo-upsell-popup-settings',
+        'woo_upsell_popup_settings_page'
     );
 }
-add_action('admin_menu', 'upsell_add_settings_page');
 
-function upsell_render_settings_page() {
+function woo_upsell_popup_settings_page() {
     ?>
     <div class="wrap">
-        <h2>Upsell Settings</h2>
-        <form action="options.php" method="post">
+        <h1>WooCommerce Upsell Popup Settings</h1>
+        <form method="post" action="options.php">
             <?php
-            settings_fields('upsell_settings_group');
-            do_settings_sections('upsell-settings');
+            settings_fields('woo-upsell-popup-settings-group');
+            do_settings_sections('woo-upsell-popup-settings');
             submit_button();
             ?>
         </form>
@@ -42,74 +42,124 @@ function upsell_render_settings_page() {
     <?php
 }
 
-// Register Settings
+add_action('admin_init', 'woo_upsell_popup_settings_init');
 
-function upsell_register_settings() {
-    register_setting('upsell_settings_group', 'upsell_custom_message');
-    register_setting('upsell_settings_group', 'upsell_product_category');
+function woo_upsell_popup_settings_init() {
+    register_setting('woo-upsell-popup-settings-group', 'woo_upsell_category');
+    register_setting('woo-upsell-popup-settings-group', 'woo_upsell_order');
+	 register_setting('woo-upsell-popup-settings-group', 'woo_upsell_message');
 
     add_settings_section(
-        'upsell_settings_section',
-        'Upsell Options',
-        'upsell_settings_section_callback',
-        'upsell-settings'
+        'woo-upsell-popup-settings-section',
+        'Settings',
+        null,
+        'woo-upsell-popup-settings'
     );
 
     add_settings_field(
-        'upsell_custom_message',
-        'Custom Message',
-        'upsell_custom_message_callback',
-        'upsell-settings',
-        'upsell_settings_section'
-    );
-
-    add_settings_field(
-        'upsell_product_category',
+        'woo_upsell_category_field',
         'Product Category',
-        'upsell_product_category_callback',
-        'upsell-settings',
-        'upsell_settings_section'
+        'woo_upsell_category_field_callback',
+        'woo-upsell-popup-settings',
+        'woo-upsell-popup-settings-section'
+    );
+
+    add_settings_field(
+        'woo_upsell_order_field',
+        'Product Order',
+        'woo_upsell_order_field_callback',
+        'woo-upsell-popup-settings',
+        'woo-upsell-popup-settings-section'
+    );
+	  add_settings_field(
+        'woo_upsell_message_field',
+        'Upsell Message',
+        'woo_upsell_message_field_callback',
+        'woo-upsell-popup-settings',
+        'woo-upsell-popup-settings-section'
     );
 }
-add_action('admin_init', 'upsell_register_settings');
-
-function upsell_settings_section_callback() {
-    echo '<p>Set your custom upsell message and choose a product category.</p>';
+function woo_upsell_message_field_callback() {
+    $message = get_option('woo_upsell_message', 'Don’t forget your accessories'); // Default message
+    ?>
+    <input type="text" id="woo_upsell_message" name="woo_upsell_message" value="<?php echo esc_attr($message); ?>">
+    <?php
+}
+function woo_upsell_category_field_callback() {
+    $selected_category = get_option('woo_upsell_category');
+    $args = array(
+        'taxonomy'   => 'product_cat',
+        'orderby'    => 'name',
+        'order'      => 'ASC',
+        'hide_empty' => false,
+    );
+    $product_categories = get_terms($args);
+    ?>
+    <select id="woo_upsell_category" name="woo_upsell_category">
+        <?php foreach ($product_categories as $category) : ?>
+            <option value="<?php echo esc_attr($category->slug); ?>" <?php selected($selected_category, $category->slug); ?>>
+                <?php echo esc_html($category->name); ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <?php
 }
 
-function upsell_custom_message_callback() {
-    $message = get_option('upsell_custom_message');
-    echo "<input type='text' name='upsell_custom_message' value='{$message}' size='50'>";
-}
 
-function upsell_product_category_callback() {
-    $selected_category = get_option('upsell_product_category');
-    $categories = get_terms('product_cat');
-    echo '<select name="upsell_product_category">';
-    foreach ($categories as $category) {
-        echo '<option value="' . $category->slug . '"' . selected($selected_category, $category->slug, false) . '>' . $category->name . '</option>';
-    }
-    echo '</select>';
+function woo_upsell_order_field_callback() {
+    $order = get_option('woo_upsell_order', 'sku');
+    ?>
+    <select id="woo_upsell_order" name="woo_upsell_order">
+        <option value="sku" <?php selected($order, 'sku'); ?>>Sort by SKU</option>
+        <option value="name" <?php selected($order, 'name'); ?>>Sort by Name</option>
+        <option value="price" <?php selected($order, 'price'); ?>>Sort by Price</option>
+        
+    </select>
+    <?php
 }
 
 // Fetch products based on a category via AJAX
 function fetch_products_by_category() {
-    $category = get_option('upsell_product_category', 'accessories');  
-    $custom_message = get_option('upsell_custom_message', 'Don’t forget your accessories'); 
+  $category = get_option('woo_upsell_category', 'accessories');  // Default to 'accessories' if not set
+$order = get_option('woo_upsell_order', 'sku'); // Default to 'sku' if not set
 
-    $products = get_posts(array(
+$query_args = array(
     'post_type' => 'product',
     'product_cat' => $category,
     'numberposts' => -1
-));
-    $output = '<h3 class="upsell">' . esc_html($custom_message) . '</h3><div class="products-carousel">';
+);
+
+// Modify query based on selected order
+switch ($order) {
+    case 'name':
+        $query_args['orderby'] = 'title';
+        $query_args['order'] = 'ASC';
+        break;
+    case 'price':
+        $query_args['orderby'] = 'meta_value_num';
+        $query_args['meta_key'] = '_price';
+        $query_args['order'] = 'ASC';
+        break;
+    case 'sku':
+    default:
+        $query_args['orderby'] = 'meta_value';
+        $query_args['meta_key'] = '_sku';
+        $query_args['order'] = 'ASC';
+        break;
+}
+
+$products = get_posts($query_args);
+
+  $upsell_message = get_option('woo_upsell_message', 'Don’t forget your accessories'); // Fetch the custom message
+
+    $output = '<h3 class="upsell">' . esc_html($upsell_message) . '</h3><div class="products-carousel">';
    foreach ($products as $product) {
-    $_product = wc_get_product($product->ID);  
+    $_product = wc_get_product($product->ID);  // Get the product object
 
     $product_permalink = get_permalink($product->ID);
     $product_title = $_product->get_title();
-    $product_image = $_product->get_image();  
-    // $product_price = $_product->get_price_html();  
+    $product_image = $_product->get_image();  // Get the product image (thumbnail)
+    $product_price = $_product->get_price_html();  // Get the product price
 
     $output .= '<div class="product-item">';
     $output .= '<a href="' . esc_url($product_permalink) . '">';
